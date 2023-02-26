8c95cae4ec7c8fadfabf6231f223762a0e69c79c
Akash <phalke.aka@gmail.com>
2023-02-26 14:38:47 +0530

    update

--- map-foreach.tf
+++ map-foreach.tf
@@ -1,14 +1,16 @@
 resource "aws_s3_bucket" "class-bucket" {
-    acl = "private"
     for_each = {
         dev = "class-ec2-2023"
         prod = "class-ec2-2023"
         test = "class-ec2-2023"
     }
-
-    bucket = ${each.key}-${each.value}
-
-    tags = {
-        env = ${each.key}-${each.value}
-    }
+ 
+    acl = "private"
+bucket = "${each.key}-${each.value}"
+   tags = {
+            enviornment = each.key
+        buckettnamee = "${each.key}-${each.value}"
+   }
 }
+
+    
