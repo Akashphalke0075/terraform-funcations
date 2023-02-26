69c4f81d016b9f64490e0a00e7b70aa213ed1465
Akash <phalke.aka@gmail.com>
2023-02-26 17:52:43 +0530

    update

--- lifecycle.tf
+++ lifecycle.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "class-server-LL" {
     instance_type = "t2.micro"
     ami = "ami-0b93ce03dcbcb10f6"
-    avaibility_zone = "us-east-1b"
+    availability_zone = "us-east-1b"
 
     tags = {
         Name = "akash"
