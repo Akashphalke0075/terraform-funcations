9a9111e482b453e0129c438241669537b898f213
Akash <phalke.aka@gmail.com>
2023-02-26 13:44:02 +0530

    update

--- instance.tf
+++ instance.tf
@@ -3,9 +3,18 @@ resource "aws_instance" "class-server-pp" {
     ami = "ami-0b93ce03dcbcb10f6"
     vpc_security_group_ids = [aws_security_group.class-security.id]
     # depends_on = [aws_security_group.class-security]
-    count = 2
+    # count = 2
 
  tags = {
- Name = "akash-server-${count.index}"
+ Name = "akash-server"
 }
+}
+
+resource "aws_instance" "class-server-pp" {
+    instance_type = "t2.micro"
+    ami = "ami-06e0ce9d3339cb039"
+    provider = "aws.eu-west-1"
+ tags = {
+    Name = "europe-inst"
+ }
 }
--- version.tf
+++ version.tf
@@ -11,3 +11,7 @@ terraform {
 provider "aws" {
     region = "us-east-1"
 }
+
+provider "aws" {
+    region = "eu-west-1"
+}
