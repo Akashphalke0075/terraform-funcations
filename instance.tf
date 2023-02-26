c5788a8ab765b04782ce6e66e5397b747f08764b
Akash <phalke.aka@gmail.com>
2023-02-26 12:08:14 +0530

    update

--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "class-ec2-server" {
     instance_type = "t2.micro"
-    ami = ["ami-0b93ce03dcbcb10f6"]
+    ami = "ami-0b93ce03dcbcb10f6"
     vpc_security_group_ids = ["aws_security_group.class-security.id"]
     depends_on = [aws_security_group.class-security]
 
