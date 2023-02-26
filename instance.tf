5c9ecb1811ccb20f5e5ee4684e43bafe88f82df0
Akash <phalke.aka@gmail.com>
2023-02-26 13:25:12 +0530

    update

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "class-server-pp" {
     instance_type = "t2.micro"
     ami = "ami-0b93ce03dcbcb10f6"
-    vpc_security_group_ids = ["aws_security_group.class-security.id"]
+    vpc_security_group_ids = [aws_security_group.class-security.id]
 
 
 }
