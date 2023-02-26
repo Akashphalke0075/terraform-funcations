a3f1ade23cd70823e0f7e433687fd7d1f4caa4b3
Akash <phalke.aka@gmail.com>
2023-02-26 17:51:31 +0530

    update

---
+++ lifecycle.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "class-server-LL" {
+    instance_type = "t2.micro"
+    ami = "ami-0b93ce03dcbcb10f6"
+    avaibility_zone = "us-east-1b"
+
+    tags = {
+        Name = "akash"
+    }
+
+}
--- map-foreach.tf
+++ map-foreach.tf
@@ -1,16 +1,16 @@
-resource "aws_s3_bucket" "class-bucket" {
-    for_each = {
-        dev = "class-ec2-2023"
-        prod = "class-ec2-2023"
-        test = "class-ec2-2023"
-    }
+# resource "aws_s3_bucket" "class-bucket" {
+#     for_each = {
+#         dev = "class-ec2-2023"
+#         prod = "class-ec2-2023"
+#         test = "class-ec2-2023"
+#     }
  
-    acl = "private"
-bucket = "${each.key}-${each.value}"
-   tags = {
-            enviornment = each.key
-        buckettnamee = "${each.key}-${each.value}"
-   }
-}
+#     acl = "private"
+# bucket = "${each.key}-${each.value}"
+#    tags = {
+#             enviornment = each.key
+#         buckettnamee = "${each.key}-${each.value}"
+#    }
+# }
 
     
