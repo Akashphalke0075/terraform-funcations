9c3ed59ab92a84e5b814ae32eb10f078f4fee482
Akash <phalke.aka@gmail.com>
2023-02-26 13:31:23 +0530

    update

--- instance.tf
+++ instance.tf
@@ -4,6 +4,6 @@ resource "aws_instance" "class-server-pp" {
     vpc_security_group_ids = [aws_security_group.class-security.id]
     depends_on = [aws_security_group.class-security]
 
- tags = 
+ tags = {
  Name = "akash-server"
 }
