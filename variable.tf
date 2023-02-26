54c749b5d089fe6a1760f2e6acd7449f9e1ed549
Akash <phalke.aka@gmail.com>
2023-02-26 18:55:11 +0530

    update

--- variable.tf
+++ variable.tf
@@ -1,19 +1,26 @@
+# variable "instance_typ" {
+#     type = map
+#     default = {
+#         dev = "t1.micro"
+#         prod = "t2.micro"
+#         test = "t3.large"
+#     }
+# }
 variable "instance_typ" {
-    type = map
-    default = {
-        dev = "t1.micro"
-        prod = "t2.micro"
-        test = "t3.large"
+    default = ["t1.micro", "t2.micro", "t3.small"]
+
+    validation {
+        condition = contains(["t1.micro", "t2.micro", "t3.small"]var.instance_typ)
+        error_message = "only free allowed"
     }
 }
-
 variable "instance_name" {
     default = "nginx"
 
 }
 
 variable "ami_name" {
-    default = "ami6-0b93ce03dcbcb10f6"
+    default = "ami-0b93ce03dcbcb10f6"
 
     validation {
         condition = length(var.ami_name)>4 && substr(var.ami_name, 0 , 4) == "ami-"
