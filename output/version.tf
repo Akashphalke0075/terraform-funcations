d10dcdd37c5d20e63cda8775ac18fb1103e549c5
Akash <phalke.aka@gmail.com>
2023-02-26 19:21:46 +0530

    update

---
+++ output/version.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_version = "~>1.3.6"
+    required_providers {
+        aws = {
+            source = "Hashicorp/aws"
+            version = "4.52.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+provider "aws" {
+    region = "eu-west-1"
+    alias = "aws-eu-west-1"
+}
