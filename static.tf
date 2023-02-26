5ff43dee9d391d86e47f38adf0e396ca175ae894
Akash <phalke.aka@gmail.com>
2023-02-26 14:03:53 +0530

    update

---
+++ static.tf
@@ -0,0 +1,9 @@
+# resource "aws_s3_bucket" "nre-bucket" {
+#     bucket = "class-nre-bucket"
+#     acl = "public read"
+
+#     website {
+#         index_document = "index.html"
+#         error_docuements = "error.html"
+#     }
+# }
