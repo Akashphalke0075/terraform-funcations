8ebc3f3afc7abd584a48785a3eb6443ceb4e8994
Akash <phalke.aka@gmail.com>
2023-02-26 14:12:13 +0530

    update

--- for-each.tf
+++ for-each.tf
@@ -4,6 +4,7 @@ variable "user-list" {
 
 resource "aws_instance" "class-ec2" {
     instance_type = "t2.micro"
+    ami = "ami-0b93ce03dcbcb10f6"
     for_each = toset(var.user-list)
     name = each.key
 }
