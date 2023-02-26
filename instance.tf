2f62fb4f24146341a2d21bc6c21dcd4dd2bbff9b
Akash <phalke.aka@gmail.com>
2023-02-26 13:34:25 +0530

    update

--- instance.tf
+++ instance.tf
@@ -2,7 +2,8 @@ resource "aws_instance" "class-server-pp" {
     instance_type = "t2.micro"
     ami = "ami-0b93ce03dcbcb10f6"
     vpc_security_group_ids = [aws_security_group.class-security.id]
-    depends_on = [aws_security_group.class-security]
+    # depends_on = [aws_security_group.class-security]
+    count = 2
 
  tags = {
  Name = "akash-server"
