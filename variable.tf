81e1c9e7bdf416bf126943ea1a20af69da558a4e
Akash <phalke.aka@gmail.com>
2023-02-26 18:49:35 +0530

    update

--- variable.tf
+++ variable.tf
@@ -13,7 +13,7 @@ variable "instance_name" {
 }
 
 variable "ami_name" {
-    default = "ami-0b93ce03dcbcb10f6"
+    default = "ami6-0b93ce03dcbcb10f6"
 
     validation {
         condition = length(var.ami_name)>4 && substr(var.ami_name, 0 , 4) == "ami-"
